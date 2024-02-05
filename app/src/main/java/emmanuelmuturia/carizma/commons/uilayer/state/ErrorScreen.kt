d47f68392d47a336d85a320cf94cfb636db3a4fb
Emmanuel Muturia <science@emmanuelmuturia.com>
2024-02-05 16:19:25 +0300

    Set up Loading Screen...

--- app/src/main/java/emmanuelmuturia/carizma/commons/uilayer/state/ErrorScreen.kt
+++ app/src/main/java/emmanuelmuturia/carizma/commons/uilayer/state/ErrorScreen.kt
@@ -1,2 +1,36 @@
 package emmanuelmuturia.carizma.commons.uilayer.state
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.unit.dp
+import emmanuelmuturia.carizma.commons.uilayer.components.CarizmaBackgroundImage
+
+@Composable
+fun ErrorScreen() {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        CarizmaBackgroundImage()
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+
+
+        }
+
+    }
+
+}
--- app/src/main/java/emmanuelmuturia/carizma/commons/uilayer/state/LoadingScreen.kt
+++ app/src/main/java/emmanuelmuturia/carizma/commons/uilayer/state/LoadingScreen.kt
@@ -4,12 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 import emmanuelmuturia.carizma.commons.uilayer.components.CarizmaBackgroundImage
 
@@ -27,10 +27,10 @@ fun LoadingScreen() {
             verticalArrangement = Arrangement.Center
         ) {
 
-            CircularProgressIndicator(
-                color = Color.White,
-                strokeWidth = 7.dp,
-                strokeCap = StrokeCap.Square
+            Text(
+                modifier = Modifier.padding(start = 7.dp, end = 7.dp),
+                text = "Uh oh! There seems to be an error there...",
+                style = MaterialTheme.typography.bodyLarge
             )
 
         }
